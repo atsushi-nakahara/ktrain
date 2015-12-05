6d65981c0e7e8913a31a6cd25cea1c143f07baa0
atsushi-nakahara <atsushi.nakahara@gmail.com>
2015-12-05 16:34:37 +0000

    update

--- ktrain.py
+++ ktrain.py
@@ -1,4 +1,13 @@
 import math
+import random
+
+_counter = 0;
+_p_idx = -1
+_log = open('./gps.log', 'r')
+
+_unit_state = [False] * 16
+_anime = 0
+_anime_type = 0
 
 #--------------------------------------------------------------------------------------------
 def getDistance(lon_a, lat_a, lon_b, lat_b):
@@ -31,20 +40,112 @@ def findPlace(lon, lat):
         d = getDistance(float(lon), float(lat), float(pos[2]), float(pos[3]));
         #print d
         if d < 200:
-            print pos[1]
+            #print pos[1]
+            return int(pos[0])
     return
-    
+
+#--------------------------------------------------------------------------------------------
+def isSwitchPressed():
+    return False
+
+#--------------------------------------------------------------------------------------------
+def animate():
+    global _unit_state
+    global _anime
+    global _anime_type
+
+    if _anime_type == 0: #wave
+        speed = 400 #todo
+        if _anime % speed == 0:
+            i = (_anime / speed) % 16
+            _unit_state[i] = True
+            i -= 3
+            if i < 0: i+16
+            _unit_state[i] = False
+    elif _anime_type == 1: #rwave
+        speed = 400 #todo
+        if _anime % speed == 0:
+            i = (_anime / speed) % 16
+            i = 15-i
+            _unit_state[i] = True
+            i -= 3
+            if i < 0: i+16
+            _unit_state[i] = False
+
+
+    #print _unit_state
+    for st in _unit_state:
+        if st == True: print '*',
+        if st == False: print '_',
+    print ''
+
+    _anime += 1
+    return
+
+#--------------------------------------------------------------------------------------------
+def checkPlace():
+    global _log
+    line = _log.readline()
+    if line:
+        vals = line.split(',')
+        return findPlace(vals[0], vals[1])
+    else :
+        return -1
+
+
+#--------------------------------------------------------------------------------------------
+def changeState():
+    global _anime
+    global _anime_type
+
+    _anime = 0
+    _anime_type = random.randint(0, 1) #todo
+    print 'changeState'
+    return
+
+#--------------------------------------------------------------------------------------------
+def playSound(_p_idx):
+    return
+
+#--------------------------------------------------------------------------------------------
+def mainloop():
+    #print 'loop'
+    global _counter
+    global _p_idx
+
+    _counter += 1
+    #print _counter
+
+    switch = isSwitchPressed()
+    if switch:
+        print 'switch pressed'
+        changeState()
+        playSound(-1)
+    else:
+        if _counter % 500 == 0: #TODO
+            #print 'check position'
+            p_idx = checkPlace()
+            if p_idx > 0 and _p_idx != p_idx:
+                _p_idx = p_idx
+                print 'place changed {0}'.format(_p_idx)
+                changeState()
+                playSound(_p_idx)
+
+    animate()
+    return
+
+#--------------------------------------------------------------------------------------------
+def init():
+    return
 
 #--------------------------------------------------------------------------------------------
 # entry point
 
-log = open('./gps.log', 'r')
+init()
+while True:
+    mainloop()
 
-for line in log:
-    #print line
-    vals = line.split(',')
-    #print vals
-    findPlace(vals[0], vals[1])
+
 
     
 
